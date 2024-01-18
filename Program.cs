7f0f9a660ecdeb8e347f18bcbf1940db9ba2c01e
HammondCameron55 <110644424+HammondCameron55@users.noreply.github.com>
2024-01-17 18:00:33 -0700

    It works!!!!

--- Program.cs
+++ Program.cs
@@ -1,2 +1,63 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading.Tasks;
+using static DiceSimulatorC_.Results;
+
+namespace DiceSimulatorC_;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Results result = new Results();
+
+        int NumRolls = 0;
+        string NumRollsString = "";
+        double[] daRollResults = new double[NumRolls];
+
+        Console.WriteLine("Welcome to the dice throwing simulator!");
+
+        //Ask user how many times they want ot roll the dice
+        Console.Write("Enter the number of times you want me to roll these two dice: ");
+        NumRollsString = Console.ReadLine();
+
+        //Converting the string received to an Integer 
+        NumRolls = Convert.ToInt32(NumRollsString);
+
+        //Printing out a string to show the user how many times the dice will be rolled
+        Console.WriteLine("For you, my fine feathered friend, I will roll these two dice " + NumRolls + " times!"); Console.WriteLine();
+        Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+        Console.WriteLine("Each '*' represents 1% of the total number of rolls."); Console.WriteLine();
+
+        //Send the number of rolls to the other class and place the response into "daRollResults"
+        daRollResults = result.RollDemDice(NumRolls);
+
+
+        //While loop to run through the whole array. uCount starts at 2 so it starts at where rolls of "2" are stored
+        int uCount = 2;        
+        while (uCount < 13)
+        {
+            //Calculate Percentage
+            double dPercent = 0.0;
+            dPercent = (daRollResults[uCount] / NumRolls) * 100 ;
+
+            //Starting line to print
+            Console.Write(uCount + ": ");
+
+            //Print the correct number of astericks 
+            for (int vCount = 0; vCount < dPercent; vCount++) 
+            {
+                Console.Write("*");
+            }
+            //New line.
+            Console.WriteLine();
+            uCount++;
+        }
+        
+
+    }
+}
+/* 
+ Console.WriteLine("Two was rolled " + daRollResults[3] + " times");
+
+ 
+ */
---
+++ Results.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceSimulatorC_
+{
+    internal class Results
+
+    {
+        //make method 
+        public double[] RollDemDice(int numRoll)
+        {
+            //Receive num rolls from main program
+            int RollNumber = numRoll;
+            int iCount = 0;
+            double[] rollTotals = new double[13];
+            Random random = new Random();
+
+            //While loop
+            while (iCount < numRoll) 
+            {
+                int idie1 = 0;
+                int idie2 = 0;
+                int irollValue = 0;
+                               
+
+                idie1 = random.Next(1, 7);
+                idie2 = random.Next(1, 7);
+                irollValue = idie1 + idie2 ;
+
+
+
+                rollTotals[irollValue]++;
+
+                iCount++;
+             }
+
+            return rollTotals; 
+
+            // rolls are done
+            
+        }
+    }
+}
+
+
+
+//Division in C#
+/*
+ integers don't have decimals 
+Just make one of them a Double 
+OR
+keep them as integars and do this:
+    int num1 = 13
+    int num2 = 100
+
+    13 * 100 / 1000
+ */
